bd471bfdbc9740f93f481a6e3dd47447068ee631
Nilesh Singh Dahiya <67730060+chhotuthakur@users.noreply.github.com>
2023-05-03 14:15:08 +0530

    update

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var config = {
+
+    apiKey: "AIzaSyA06MYkv3ai7xa3hYaj17iTP83khWdL8Ac",
+            authDomain: "money-market-1b4b2.firebaseapp.com",
+            databaseURL: "https://money-market-1b4b2-default-rtdb.firebaseio.com",
+            projectId: "money-market-1b4b2",
+            storageBucket: "money-market-1b4b2.appspot.com",
+            messagingSenderId: "827481462207",
+            appId: "1:827481462207:web:4cb149cf0bce5b00e83d0a",
+            measurementId: "G-Y29EZBD948"
+
+
+
+    
+  };
+  firebase.initializeApp(config);
